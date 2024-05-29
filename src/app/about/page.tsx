b37447c5969848f11a1101c48f2340ff9675ce96
ofeklevy11 <ofeklevy53@gmail.com>
2024-05-29 18:17:50 +0300

    a

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
           engineer, I thrive on creating innovative solutions and pushing the
           boundaries of web development. With a deep understanding of modern
           technologies and a commitment to continuous learning, I bring a blend
-          of creativity and technical expertise to every project. Let&apos;s build
+          of creativity and technical expertise to every project. Let&apos;s builda
           the future together, one line of code at a time. 🌐💻✨
         </p>
         <p className="leading-6 tracking-wide max-w-[600px] mx-auto">
